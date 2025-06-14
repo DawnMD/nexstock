38866781efe044d6121f63e959838d1387325f1b
SyedSibtainRazvi <syedsibtain191@gmail.com>
2025-06-14 16:41:38 +0530

    refactor: update Vendor and Order models to use Int for IDs, simplify field types, and adjust seed data references

--- prisma/migrations/20250614110924_initial_schema/migration.sql
+++ prisma/migrations/20250614110924_initial_schema/migration.sql
@@ -3,9 +3,9 @@ CREATE TYPE "OrderStatus" AS ENUM ('NEW', 'IN_PROGRESS', 'COMPLETED', 'CANCELLED
 
 -- CreateTable
 CREATE TABLE "Vendor" (
-    "id" BIGSERIAL NOT NULL,
-    "name" VARCHAR(100) NOT NULL,
-    "reference" VARCHAR(50) NOT NULL,
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "reference" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -14,23 +14,23 @@ CREATE TABLE "Vendor" (
 
 -- CreateTable
 CREATE TABLE "Order" (
-    "id" BIGSERIAL NOT NULL,
-    "orderNumber" VARCHAR(50) NOT NULL,
-    "businessUnit" VARCHAR(50) NOT NULL,
+    "id" SERIAL NOT NULL,
+    "orderNumber" TEXT NOT NULL,
+    "businessUnit" TEXT NOT NULL,
     "purchaseOrderType" INTEGER NOT NULL,
     "purchaseOrderDate" TIMESTAMP(3),
     "expectedReceiptDate" TIMESTAMP(3),
     "status" "OrderStatus" NOT NULL DEFAULT 'NEW',
     "notes" TEXT,
-    "vendorReference" VARCHAR(50) NOT NULL,
-    "vendorName" VARCHAR(100) NOT NULL,
-    "buyerName" VARCHAR(100),
-    "buyerCity" VARCHAR(100),
+    "vendorReference" TEXT NOT NULL,
+    "vendorName" TEXT NOT NULL,
+    "buyerName" TEXT,
+    "buyerCity" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "createdBy" VARCHAR(50) NOT NULL,
+    "createdBy" TEXT NOT NULL,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "updatedBy" VARCHAR(50) NOT NULL,
-    "paymentStatus" VARCHAR(50),
+    "updatedBy" TEXT NOT NULL,
+    "paymentStatus" TEXT,
 
     CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
 );
@@ -42,16 +42,7 @@ CREATE UNIQUE INDEX "Vendor_reference_key" ON "Vendor"("reference");
 CREATE UNIQUE INDEX "Order_orderNumber_key" ON "Order"("orderNumber");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Order_vendorReference_key" ON "Order"("vendorReference");
-
--- CreateIndex
-CREATE INDEX "Order_vendorReference_idx" ON "Order"("vendorReference");
-
--- CreateIndex
-CREATE INDEX "Order_orderNumber_idx" ON "Order"("orderNumber");
-
--- CreateIndex
-CREATE INDEX "Order_status_idx" ON "Order"("status");
+CREATE INDEX "Order_vendorReference_orderNumber_status_idx" ON "Order"("vendorReference", "orderNumber", "status");
 
 -- AddForeignKey
 ALTER TABLE "Order" ADD CONSTRAINT "Order_vendorReference_fkey" FOREIGN KEY ("vendorReference") REFERENCES "Vendor"("reference") ON DELETE RESTRICT ON UPDATE CASCADE;
