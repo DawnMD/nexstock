67e94851b2c4a386ed5f39dddeb7081b205476fd
Mainak Das <mainakdas104@gmail.com>
2025-06-22 10:54:58 +0530

    feat: enhance OrdersLoading component with search input and button for improved order filtering experience

--- app/(main-app)/orders/page.tsx
+++ app/(main-app)/orders/page.tsx
@@ -1,8 +1,21 @@
 import { OrderTable } from "@/components/order-table";
 import { api, HydrateClient } from "@/trpc/server";
 
-export default function Page() {
-  void api.order.getPaginatedOrders.prefetch({ limit: 20, pageIndex: 0 });
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{
+    page?: string;
+    limit?: string;
+    query?: string;
+  }>;
+}) {
+  const { query } = await searchParams;
+  void api.order.getPaginatedOrders.prefetch({
+    limit: 20,
+    pageIndex: 0,
+    search: query,
+  });
 
   return (
     <HydrateClient>
