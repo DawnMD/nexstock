a7bb226d802b565153e42129782a84f9cffdec91
Mainak Das <mainakdas104@gmail.com>
2025-06-23 23:34:50 +0530

    feat: add next.config.js for Next.js configuration and remove TypeScript config file

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import "./src/env.js";
+
+/** @type {import("next").NextConfig} */
+const config = {
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true,
+      },
+    ];
+  },
+};
+
+export default config;
