dbac6c92b8561fc3d2036d3222e146e422696992
SyedSibtainRazvi <syedsibtain191@gmail.com>
2025-06-24 19:33:45 +0530

    fix: update navigation item matching logic in SiteHeader component

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -5,7 +5,14 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 import { usePathname } from "next/navigation";
 import { data } from "@/components/app-sidebar";
 
-export function SiteHeader({ title }: { title: string }) {
+export function SiteHeader() {
+  const path = usePathname();
+  const pathname = path.split("/")[1];
+  const currentPageName = data.navMain.find(
+    (item) => item.url === `/${pathname}`,
+  )?.title;
+  console.log(pathname);
+
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
