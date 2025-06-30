75cd1e4acffe8eb892dcacdfd4e357c04a4bba53
Mainak Das <mainakdas104@gmail.com>
2025-06-30 17:02:55 +0530

    feat: implement CheckOutPage and CheckOutLoading components for vehicle check-out process with skeleton loading states

---
+++ app/(main-app)/dock-booking/[orderNumber]/[vehicleNumber]/check-out/page.tsx
@@ -0,0 +1,43 @@
+import { SiteHeader } from "@/components/site-header";
+import { VehicleActivityContainer } from "@/components/vehicle-activity-containner";
+import { VehicleActivityForm } from "@/components/vehicle-activity-form";
+import { api, HydrateClient } from "@/trpc/server";
+import { notFound } from "next/navigation";
+
+export default async function CheckOutPage({
+  params,
+}: {
+  params: Promise<{ orderNumber: string; vehicleNumber: string }>;
+}) {
+  const { orderNumber, vehicleNumber } = await params;
+
+  const dockBookingDetails =
+    await api.order.getDockBookingByVehicleNumberAndOrderNumber({
+      vehicleNumber,
+      orderNumber,
+    });
+
+  if (!dockBookingDetails) {
+    notFound();
+  }
+
+  return (
+    <>
+      <SiteHeader title="Vehicle Check Out" />
+      <main className="p-4">
+        <VehicleActivityContainer
+          dockBookingDetails={dockBookingDetails}
+          orderNumber={orderNumber}
+          vehicleNumber={vehicleNumber}
+        >
+          <HydrateClient>
+            <VehicleActivityForm
+              vehicleNumber={vehicleNumber}
+              activityType="CHECK_OUT"
+            />
+          </HydrateClient>
+        </VehicleActivityContainer>
+      </main>
+    </>
+  );
+}
