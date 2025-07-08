1a0015f677fa3b764423106adfded91ee74f0874
Mainak Das <mainakdas104@gmail.com>
2025-07-08 19:29:51 +0530

    feat: enhance QualityCheckItems component with quality check status display and action buttons

--- components/quality-check-items.tsx
+++ components/quality-check-items.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -8,9 +9,14 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Badge } from "@/components/ui/badge";
 import { api } from "@/trpc/react";
-import { ClipboardCheckIcon, PackageIcon } from "lucide-react";
+import {
+  CheckIcon,
+  ClipboardCheckIcon,
+  PackageIcon,
+  RotateCcwIcon,
+} from "lucide-react";
+import Link from "next/link";
 
 export function QualityCheckItems({ orderNumber }: { orderNumber: string }) {
   const [orderItems] = api.qualityCheck.getOrderItems.useSuspenseQuery({
@@ -33,17 +39,19 @@ export function QualityCheckItems({ orderNumber }: { orderNumber: string }) {
 
   return (
     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-      {orderItems.items.map((sku) => (
-        <Card key={sku.Sku.sku} className="relative">
+      {orderItems.items.map((item) => (
+        <Card key={item.Sku.sku} className="relative">
           <CardHeader className="flex items-center gap-2 pb-3">
             <div className="bg-primary/10 flex h-8 w-8 items-center justify-center rounded-lg">
               <PackageIcon className="text-primary h-4 w-4" />
             </div>
 
             <div>
-              <CardTitle className="font-mono text-sm">{sku.Sku.sku}</CardTitle>
+              <CardTitle className="font-mono text-sm">
+                {item.Sku.sku}
+              </CardTitle>
               <CardDescription className="text-xs">
-                {sku.Sku.description}
+                {item.Sku.description}
               </CardDescription>
             </div>
           </CardHeader>
@@ -51,22 +59,58 @@ export function QualityCheckItems({ orderNumber }: { orderNumber: string }) {
             {/* SKU Details */}
             <div className="space-y-2 text-xs">
               <div className="flex justify-between">
-                <span className="text-muted-foreground">Category:</span>
-                <Badge variant="outline" className="text-xs">
-                  {sku.Sku.department}
+                <span className="text-muted-foreground">QC status:</span>
+                <Badge
+                  variant={
+                    item.qualityCheck?.qualityCheckStatus ? "green" : "blue"
+                  }
+                  className="text-xs"
+                >
+                  {item.qualityCheck?.qualityCheckStatus ? "Done" : "Not Done"}
                 </Badge>
               </div>
               <div className="flex justify-between">
-                <span className="text-muted-foreground">Supplier:</span>
-                <span className="font-medium">{orderItems.vendor.name}</span>
+                <span className="text-muted-foreground">Category:</span>
+                <Badge variant="outline" className="text-xs">
+                  {item.Sku.department}
+                </Badge>
+              </div>
+              <div className="flex justify-between">
+                <span className="text-muted-foreground">Ordered:</span>
+                <span className="font-medium">{item.orderedQuantity}</span>
               </div>
             </div>
             {/* Action Button */}
             <div className="pt-2">
-              <Button size="sm" className="w-full">
-                <ClipboardCheckIcon className="mr-2 h-3 w-3" />
-                Start QC
-              </Button>
+              {!item.qualityCheck?.qualityCheckStatus && (
+                <Button size="sm" className="w-full cursor-pointer" asChild>
+                  <Link href={`/quality-check/${orderNumber}/${item.id}`}>
+                    <ClipboardCheckIcon className="mr-2 h-3 w-3" />
+                    Start QC
+                  </Link>
+                </Button>
+              )}
+              {item.qualityCheck?.qualityCheckStatus && (
+                <div className="flex flex-col gap-2 lg:flex-row lg:items-center">
+                  <Button
+                    size="sm"
+                    className="cursor-pointer lg:basis-1/2"
+                    variant="outline"
+                    disabled
+                  >
+                    <CheckIcon className="mr-2 h-3 w-3" />
+                    Completed
+                  </Button>
+                  <Button
+                    size="sm"
+                    className="cursor-pointer lg:basis-1/2"
+                    variant={"destructive"}
+                  >
+                    <RotateCcwIcon className="mr-2 h-3 w-3" />
+                    Reset QC
+                  </Button>
+                </div>
+              )}
             </div>
           </CardContent>
         </Card>
