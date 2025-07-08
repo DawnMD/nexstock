0e22dc2c8586f0364052df8dd27fbe7c291f2021
Mainak Das <mainakdas104@gmail.com>
2025-07-08 20:13:12 +0530

    refactor: remove disabled state logic for order items in QualityCheckOrderSearch component

--- components/quality-check-order-search.tsx
+++ components/quality-check-order-search.tsx
@@ -26,9 +26,6 @@ export function QualityCheckOrderSearch() {
         <CommandGroup heading="Order Numbers">
           {orders.map((order) => (
             <CommandItem
-              disabled={order.items.every(
-                (item) => item.qualityCheck?.qualityCheckStatus,
-              )}
               key={order.orderNumber}
               value={order.orderNumber}
               onSelect={() =>
