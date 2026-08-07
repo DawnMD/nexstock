ff060dbd7ffe4d27d7f38fa8f5ef884fefdf443c
Mainak Das <mainakdas104@gmail.com>
2026-08-07 22:22:54 +0530

    fix: correct environment variable reference in getBaseUrl function

--- lib/get-base-url.ts
+++ lib/get-base-url.ts
@@ -2,6 +2,6 @@ import { env } from "@/env";
 
 export const getBaseUrl = () => {
   if (typeof window !== "undefined") return window.location.origin;
-  if (process.env.VERCEL_URL) return `https://${env.VERCEL_URL}`;
+  if (env.VERCEL_URL) return `https://${env.VERCEL_URL}`;
   return `http://localhost:${process.env.PORT ?? 3000}`;
 };
